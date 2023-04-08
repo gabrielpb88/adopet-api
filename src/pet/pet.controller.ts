7d81441fc720133f25e9b676f0feb64e97b95972
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2023-04-08 19:44:35 -0300

    docs: add swagger docs to endpoints

--- src/pet/pet.controller.ts
+++ src/pet/pet.controller.ts
@@ -3,7 +3,9 @@ import { CreatePetDto } from './dto/create-pet.dto';
 import { PetService } from './pet.service';
 import { Pet } from './pet.entity';
 import { UpdatePetDto } from './dto/update-pet.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Pet')
 @Controller('pet')
 export class PetController {
   constructor(
@@ -11,26 +13,31 @@ export class PetController {
     private readonly service: PetService,
   ) {}
 
+  @ApiOperation({ summary: 'Get all Pets' })
   @Get()
   async getAll(): Promise<Pet[]> {
     return this.service.getAll();
   }
 
+  @ApiOperation({ summary: 'Get single Pet by id' })
   @Get(':id')
   async getById(@Param('id', ParseIntPipe) id: number): Promise<Pet> {
     return await this.service.findById(id);
   }
 
+  @ApiOperation({ summary: 'Creates a new Pet' })
   @Post()
   async create(@Body() createPetDto: CreatePetDto): Promise<Pet> {
     return this.service.create(createPetDto);
   }
 
+  @ApiOperation({ summary: 'Update an Pet' })
   @Put(':id')
   async update(@Param('id', ParseIntPipe) id: number, @Body() updatePetDto: UpdatePetDto): Promise<Pet> {
     return this.service.update(id, updatePetDto);
   }
 
+  @ApiOperation({ summary: 'Removes Pet by id' })
   @Delete(':id')
   async remove(@Param('id', ParseIntPipe) id: number): Promise<Pet> {
     return await this.service.remove(id);
--- src/tutor/tutor.controller.ts
+++ src/tutor/tutor.controller.ts
@@ -3,31 +3,38 @@ import { TutorService } from './tutor.service';
 import { CreateTutorDto } from './dto/create-tutor.dto';
 import { UpdateTutorDto } from './dto/update-tutor.dto';
 import { Tutor } from './tutor.entity';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Tutor')
 @Controller('tutor')
 export class TutorController {
   constructor(private readonly tutorService: TutorService) {}
 
+  @ApiOperation({ summary: 'Creates a new Tutor' })
   @Post()
   async create(@Body() createTutorDto: CreateTutorDto): Promise<Tutor> {
     return this.tutorService.create(createTutorDto);
   }
 
+  @ApiOperation({ summary: 'Get all tutors' })
   @Get()
   async findAll(): Promise<Tutor[]> {
     return this.tutorService.findAll();
   }
 
+  @ApiOperation({ summary: 'Get single tutor by id' })
   @Get(':id')
   async findOne(@Param('id', ParseIntPipe) id: number): Promise<Tutor> {
     return this.tutorService.findOne(id);
   }
 
+  @ApiOperation({ summary: 'Update tutor' })
   @Put(':id')
   async update(@Param('id', ParseIntPipe) id: number, @Body() updateTutorDto: UpdateTutorDto): Promise<Tutor> {
     return this.tutorService.update(id, updateTutorDto);
   }
 
+  @ApiOperation({ summary: 'Removes tutor' })
   @Delete(':id')
   async remove(@Param('id', ParseIntPipe) id: number): Promise<Tutor> {
     return await this.tutorService.remove(id);
