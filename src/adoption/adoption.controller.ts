8dacdca8f2867ec104ec60495a85acea39af5315
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2023-04-13 18:17:01 -0300

    feat: create endpoint to make an adoption

--- src/adoption/adoption.controller.ts
+++ src/adoption/adoption.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Inject, Post, UseGuards } from '@nestjs/common';
+import { CreateAdoptionDto } from './dto/create-adoption.dto';
+import { User } from '../auth/auth.entity';
+import { Adoption } from './adoption.entity';
+import { GetUser } from '../auth/user.decorator';
+import { AdoptionService } from './adoption.service';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from '../auth/roles.guard';
+import { Roles } from '../auth/roles.decorator';
+import { Role } from '../enums/role.enum';
 
 @Controller('adoption')
-export class AdoptionController {}
+export class AdoptionController {
+  constructor(
+    @Inject(AdoptionService)
+    private readonly service: AdoptionService,
+  ) {}
+
+  @ApiOperation({ summary: 'Creates an Adoption' })
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard(), RolesGuard)
+  @Roles(Role.Employee)
+  @Post()
+  async adopt(@Body() dto: CreateAdoptionDto, @GetUser() loggedUser: User): Promise<Adoption> {
+    return this.service.adopt(dto, loggedUser);
+  }
+}
--- src/adoption/adoption.module.ts
+++ src/adoption/adoption.module.ts
@@ -6,9 +6,10 @@ import { Adoption } from './adoption.entity';
 import { TutorModule } from '../tutor/tutor.module';
 import { PetModule } from '../pet/pet.module';
 import { EmployeeModule } from '../employee/employee.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Adoption]), TutorModule, PetModule, EmployeeModule],
+  imports: [TypeOrmModule.forFeature([Adoption]), TutorModule, PetModule, EmployeeModule, AuthModule],
   controllers: [AdoptionController],
   providers: [AdoptionService],
 })
